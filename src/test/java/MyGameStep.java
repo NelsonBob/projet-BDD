4fc69b6ad37ee608d0e273c3a03e98ddb37c2679
Nelson <nels.fomekong@gmail.com>
2022-02-14 18:09:51 +0100

    Ajout des modif

--- src/test/java/MyGameStep.java
+++ src/test/java/MyGameStep.java
@@ -4,8 +4,8 @@ import io.cucumber.java.en.When;
 
 public class MyGameStep {
 
-        @Given("{string} roll the dice and has score {int}" )
-        public void thePlayerRollTheDice(String playerName , int score ) {
+        @Given("{string} roll the dice and has score {string}" )
+        public void thePlayerRollTheDiceAndHasScore(String playerName,String score) {
         }
         @When( "{string} has a higher score than {string}")
         public void  theFirstPlayersHasHigherScoreThanSecondPlayer(String player1Name,String player2Name){
@@ -16,4 +16,30 @@ public class MyGameStep {
 
         }
 
+        @Given("{string} roll the dice and has a score {string}" )
+        public void thePlayerRollTheDiceAndHasAScore(String playerName , String score ) {
+        }
+        @When( "{string} has a higher score than in {string}")
+        public void  theFirstPlayersHasHigherScoreThanInSecondPlayer(String player1Name,String player2Name){
+
+        }
+
+        @Then("{string} should Win the {string}")
+        public void shouldWinThe(String player1Name, String  player2Name ) {
+        }
+
+
+        @Given("{string} roll dice and has score {string}")
+        public void thePlayerRollDiceAndHasScore(String playerName, String score) {
+        }
+
+        @When("{string} has a equal score than {string}")
+        public void theFirstPlayersHasAEqualScoreThanSecondPlayer(String player1Name, String player2Name) {
+        }
+
+
+
+        @Then("{string} should continue to date with {string}")
+        public void heFirstPlayersShouldContinueToDateWithSecondPlayer(String player1Name, String arplayer2Nameg) {
+        }
 }
